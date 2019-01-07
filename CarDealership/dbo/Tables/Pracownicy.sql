5eddc9359271e96de2d2428f05afe991b98a3943
Anna Bracha <anna.bracha.91@gmail.com>
2019-01-07 20:50:15 +0100

    Upload DB

---
+++ CarDealership/dbo/Tables/Pracownicy.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Pracownicy] (
+    [IdPracownik]      INT            IDENTITY (1, 1) NOT NULL,
+    [Imie]             NVARCHAR (80)  NOT NULL,
+    [Nazwisko]         NVARCHAR (150) NOT NULL,
+    [PESEL]            CHAR (11)      NULL,
+    [Adres]            NVARCHAR (300) NULL,
+    [Mail]             NVARCHAR (100) NULL,
+    [Telefon]          CHAR (9)       NULL,
+    [DataZatrudnienia] DATE           NULL,
+    PRIMARY KEY CLUSTERED ([IdPracownik] ASC),
+    CHECK ([DataZatrudnienia]<=getdate()),
+    UNIQUE NONCLUSTERED ([PESEL] ASC)
+);
+
