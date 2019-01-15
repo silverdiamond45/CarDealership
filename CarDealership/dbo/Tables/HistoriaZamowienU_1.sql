d0ca8dcc3d1a671643c71ba17eb4fb8af2cbfa47
Anna Bracha <anna.bracha.91@gmail.com>
2019-01-15 12:40:27 +0100

    added/modified foreign keys

---
+++ CarDealership/dbo/Tables/HistoriaZamowienU_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[HistoriaZamowienU] (
+    [IdZmianaStatusu] INT  IDENTITY (1, 1) NOT NULL,
+    [IdStatus]        INT  NOT NULL,
+    [IdPracownik]     INT  NULL,
+    [IdZamowienie]    INT  NOT NULL,
+    [DataZmiany]      DATE DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([IdZmianaStatusu] ASC)
+);
+
